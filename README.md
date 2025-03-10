5112df870c9fa22ceab0ef701c17d45599cfefc4
Sebastian Rojas Vargas <sebasrv26@gmail.com>
2025-03-10 12:54:53 -0600

    Clone an astro portfolio template

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# SebastianRV26.github.io
+# Portfolio
--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const emailJSConfig = {
-  serviceId: process.env.REACT_APP_EMAILJS_SERVICE_ID,
-  templateId: process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-  userId: process.env.REACT_APP_EMAILJS_USER_ID,
-};
