db75485eb4d6b355e12ae28a7b02b4e9a6677e70
Sebastian Rojas Vargas <sebasrv26@gmail.com>
2025-03-13 21:58:44 -0600

    Remove action settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  env: {
-    schema: {
-      RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
-      MY_EMAIL: envField.string({ context: "server", access: "secret" }),
-    },
-  },
-  output: "server",
   adapter: vercel(),
 });
