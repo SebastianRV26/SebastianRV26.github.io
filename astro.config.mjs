5fbf77ea4d9ca83e9967a408db592f5ff5edc052
Sebastian Rojas Vargas <sebasrv26@gmail.com>
2025-03-23 19:06:51 -0600

    Add deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  site: 'https://sebastianrv26.github.io',
+  // base: '', // my-repo
   vite: {
     plugins: [tailwindcss()],
   },
